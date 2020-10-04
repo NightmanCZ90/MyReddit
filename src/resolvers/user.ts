547fc1cb87a2d673a205ebdbfdf0c5b4844cb1d4
NightmanCZ90 <novak.jan90@gmail.com>
2020-10-04 23:00:54 +0200

    set up cookie sessions

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,4 +1,4 @@
-import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Resolver } from "type-graphql";
+import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Query, Resolver } from "type-graphql";
 import { MyContext } from "../types";
 import { User } from "../entities/User";
 import argon2 from "argon2";
@@ -30,27 +30,42 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+  @Query(() => User, {nullable: true})
+  async me(@Ctx() { req, em }: MyContext) {
+    // you are not logged in
+    if (!req.session.userId) {
+      return null
+    }
+
+    const user = await em.findOne(User, {id: req.session.userId});
+    return user;
+  }
+
   @Mutation(() => UserResponse)
   async register(
     @Arg('options') options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext
+    @Ctx() { em, req }: MyContext
   ): Promise<UserResponse> {
 
     if (options.username.length <= 2) {
       return {
-        errors: [{
-          field: "username",
-          message: "username must be larger than 2 characters"
-        }]
+        errors: [
+          {
+            field: "username",
+            message: "username must be larger than 2 characters"
+          }
+        ]
       }
     }
 
     if (options.password.length <= 7) {
       return {
-        errors: [{
-          field: "password",
-          message: "password must be at least 8 characters"
-        }]
+        errors: [
+          {
+            field: "password",
+            message: "password must be at least 8 characters"
+          }
+        ]
       }
     }
 
@@ -66,39 +81,53 @@ export class UserResolver {
       // duplicate username error
       if (err.code === "23505") {
         return {
-          errors: [{
-            field: "username",
-            message: "username already taken"
-          }]
+          errors: [
+            {
+              field: "username",
+              message: "username already taken"
+            }
+          ]
         }
       }
     }
+    // store user id session
+    // this will set a cookie on the user
+    // keep them logged in
+    req.session.userId = user.id;
+
     return { user };
   }
 
   @Mutation(() => UserResponse)
   async login(
     @Arg('options') options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext
+    @Ctx() { em, req }: MyContext
   ): Promise<UserResponse> {
     const user = await em.findOne(User, { username: options.username });
     if (!user) {
       return {
-        errors: [{
-          field: "username",
-          message: "that username does not exist"
-        }]
+        errors: [
+          {
+            field: "username",
+            message: "that username does not exist"
+          }
+        ]
       }
     }
     const valid = await argon2.verify(user.password, options.password);
     if (!valid) {
       return {
-        errors: [{
-          field: "password",
-          message: "incorrect password"
-        }]
+        errors: [
+          {
+            field: "password",
+            message: "incorrect password"
+          }
+        ]
       }
     }
+
+    req.session.userId = user.id;
+
     return {
       user,
     }
